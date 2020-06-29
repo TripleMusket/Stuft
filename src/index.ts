0b48ae4e89f3a45b16b9eb9128ed42a71734925d
RickyC0626 <rickychon99@gmail.com>
2020-06-28 22:04:15 -0400

    Initial Student class with Entity class and interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+import { Student } from "./Student/Student";
+
+let entity = new Student("John Doe");
+console.log(`An entity of type ${entity.getType()} is named ${entity.getName()}`);
+
+entity.setName("Don Joe");
+console.log(`The entity's name is now ${entity.getName()}`);
